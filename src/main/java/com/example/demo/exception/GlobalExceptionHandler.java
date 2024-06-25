cfb4ea0ee16c0c2becb3f293d20be05714fa3bf8
jabell0310 <jabell0310@naver.com>
2024-06-25 11:46:08 +0900

    fix: 전역예외처리 메서드 자바 컨벤션에 맞게 수정

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -9,55 +9,55 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RequestNullExistException.class)
-    public ResponseEntity<ErrorResponse> RequestNullExistException() {
+    public ResponseEntity<ErrorResponse> requestNullExistException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.REQUEST_NULL_VALUE);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(400));
     }
 
     @ExceptionHandler(MemberNotExistException.class)
-    public ResponseEntity<ErrorResponse> MemberNotExistException() {
+    public ResponseEntity<ErrorResponse> memberNotExistException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.MEMBER_NOT_EXIST);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(400));
     }
 
     @ExceptionHandler(BoardNotExistException.class)
-    public ResponseEntity<ErrorResponse> BoardNotExistException() {
+    public ResponseEntity<ErrorResponse> boardNotExistException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.BOARD_NOT_EXIST);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(400));
     }
 
     @ExceptionHandler(ArticleNotFoundException.class)
-    public ResponseEntity<ErrorResponse> ArticleNotFoundException() {
+    public ResponseEntity<ErrorResponse> articleNotFoundException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.ARTICLE_NOT_EXIST);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(404));
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
-    public ResponseEntity<ErrorResponse> MemberNotFoundException() {
+    public ResponseEntity<ErrorResponse> memberNotFoundException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.MEMBER_NOT_FOUND);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(404));
     }
 
     @ExceptionHandler(BoardNotFoundException.class)
-    public ResponseEntity<ErrorResponse> BoardNotFoundException() {
+    public ResponseEntity<ErrorResponse> boardNotFoundException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.BOARD_NOT_FOUND);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(404));
     }
 
     @ExceptionHandler(AlreadyHasEmailException.class)
-    public ResponseEntity<ErrorResponse> AlreadyHasEmailException() {
+    public ResponseEntity<ErrorResponse> alreadyHasEmailException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.EMAIL_EXIST);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(409));
     }
 
     @ExceptionHandler(MemberHasArticleException.class)
-    public ResponseEntity<ErrorResponse> MemberHasArticleException() {
+    public ResponseEntity<ErrorResponse> memberHasArticleException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.MEMBER_ARTICLE_EXIST);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(400));
     }
 
     @ExceptionHandler(BoardHasArticleException.class)
-    public ResponseEntity<ErrorResponse> BoardHasArticleException() {
+    public ResponseEntity<ErrorResponse> boardHasArticleException() {
         ErrorResponse response = new ErrorResponse(ErrorCode.BOARD_ARTICLE_EXIST);
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(400));
     }
